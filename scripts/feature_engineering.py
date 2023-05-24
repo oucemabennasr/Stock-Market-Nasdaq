6c6ec7ad839109e5d5624d7f4d1c54c31bc70b69
Oussama Ben Nasr <96354393+oucemabennasr@users.noreply.github.com>
2023-05-24 14:21:10 -0400

    Update feature_engineering.py

--- scripts/feature_engineering.py
+++ scripts/feature_engineering.py
@@ -1,11 +1,19 @@
-import pyspark
+import argparse
 import pandas as pd
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import col, to_date, avg, median
+from pyspark.sql.functions import col, to_date, avg
 from pyspark.sql.pandas.functions import pandas_udf, PandasUDFType
 from pyspark.sql.types import StringType
 from pyspark.sql.window import Window
 
+
+parser = argparse.ArgumentParser(description='Process stock market data files.')
+parser.add_argument('directory', type=str, help='Directory path containing the data parquet files')
+parser.add_argument('data', type=str, choices=['etfs', 'stocks'], help='Data to process (etfs or stocks)')
+
+args = parser.parse_args()
+directory_path = args.directory
+data = args.data
 
 spark = SparkSession.builder.appName("FeatureEngineering")\
     .config("spark.executor.instances", "1") \
@@ -18,29 +26,18 @@ spark = SparkSession.builder.appName("FeatureEngineering")\
 
 window_spec = Window.partitionBy("Symbol").orderBy("Date").rowsBetween(-29, 0)
 
+
 @pandas_udf("float", PandasUDFType.GROUPED_AGG)
 def calculate_median(arr):
     arr_copy = arr.copy()
     return arr_copy.median()
 
-df_etfs = spark.read.parquet("/home/cloud_user/Stock-Market-Nasdaq/data/parquet_file/etfs.parquet")
-df_stocks = spark.read.parquet("/home/cloud_user/Stock-Market-Nasdaq/data/parquet_file/stocks.parquet")
 
+df = spark.read.parquet(os.path.join(directory_path, f'{data}.parquet')
+                        
+df = df.withColumn("Date", to_date("Date", "yyyy-MM-dd"))
+df = df.withColumn("vol_moving_avg", avg("Volume").over(window_spec))
+df = df.withColumn("adj_close_rolling_med", calculate_median(col("Adj_Close")).over(window_spec))
+df = df.withColumn("Date", col("Date").cast(StringType()))
 
-df_etfs = df_etfs.withColumn("Date", to_date("Date", "yyyy-MM-dd"))
-df_stocks = df_stocks.withColumn("Date", to_date("Date", "yyyy-MM-dd"))
-
-
-df_etfs = df_etfs.withColumn("vol_moving_avg", avg("Volume").over(window_spec))
-df_stocks = df_stocks.withColumn("vol_moving_avg", avg("Volume").over(window_spec))
-
-
-df_etfs = df_etfs.withColumn("adj_close_rolling_med", calculate_median(col("Adj_Close")).over(window_spec))
-df_stocks = df_stocks.withColumn("adj_close_rolling_med", calculate_median(col("Adj_Close")).over(window_spec))
-
-
-df_etfs = df_etfs.withColumn("Date", col("Date").cast(StringType()))
-df_stocks = df_stocks.withColumn("Date", col("Date").cast(StringType()))
-
-df_etfs.write.parquet("/home/cloud_user/Stock-Market-Nasdaq/FeatureEngineeringData/etfs.parquet")
-df_stocks.write.parquet("/home/cloud_user/Stock-Market-Nasdaq/FeatureEngineeringData/stocks.parquet")
+df.write.parquet("/home/cloud_user/Stock-Market-Nasdaq/FeatureEngineeringData/etfs.parquet")
