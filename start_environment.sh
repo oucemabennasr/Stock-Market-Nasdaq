a9e271c7e30bffee9043342830e546e66284c944
oucemabennasr <cloud_user@4b5d28de6f1c.mylabserver.com>
2023-05-24 19:46:44 +0000

    Add start_environment.sh

---
+++ start_environment.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt-get install python3 python3-dev
+sudo apt-get install openjdk-17-jdk
+sudo apt install python3-pip
+sudo pip3 install pandas
+sudo pip3 install pyarrow
+sudo pip3 install pyspark
