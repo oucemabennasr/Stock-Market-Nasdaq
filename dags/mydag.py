feb5a5018fd09b217a5bb469b662b0f7d28f9c93
Oussama Ben Nasr <96354393+oucemabennasr@users.noreply.github.com>
2023-05-28 05:39:05 -0400

    Update mydag.py

--- dags/mydag.py
+++ dags/mydag.py
@@ -1,27 +1,40 @@
-import logging
+  GNU nano 4.8                                                                                                                                                                                                                                                                                                    airflow/dags/mydag.py                                                                                                                                                                                                                                                                                                              import logging
+import airflow
 from datetime import datetime, timedelta
 from airflow import DAG
 from airflow.providers.apache.spark.operators.spark_submit import SparkSubmitOperator
 from airflow.operators.bash_operator import BashOperator
-
+from airflow.utils.dates import days_ago
 default_args = {
-    'start_date': datetime(2023, 6, 1),
-    'retries': 3,
-    'retry_delay': timedelta(minutes=1),
+    'owner': 'airflow',
+    #'start_date': airflow.utils.dates.days_ago(2),
+    # 'end_date': datetime(),
+    # 'depends_on_past': False,
+    # 'email': ['airflow@example.com'],
+    # 'email_on_failure': False,
+    #'email_on_retry': False,
+    # If a task fails, retry it once after waiting
+    # at least 5 minutes
+    #'retries': 1,
+    'retry_delay': timedelta(minutes=5),
 }
-
 dag = DAG(
-    'stock_market_dag',
+    dag_id='stock_market_dag',
     default_args=default_args,
+    start_date = airflow.utils.dates.days_ago(1),
+    dagrun_timeout=timedelta(minutes=60),
+    schedule_interval='@once',
     description='DAG for stock market data processing and machine learning training',
 )
 
-scripts_dir = '/home/cloud_user/airflow/Stock-Market-Nasdaq/scripts'
-data_dir = '/home/cloud_user/airflow/Stock-Market-Nasdaq/data'
+scripts_dir = '/home/cloud_user/Stock-Market-Nasdaq/scripts'
+data_dir = '/home/cloud_user/Stock-Market-Nasdaq/data'
 parquet_file_path = f'{data_dir}/parquet_file'
+output_dir = f'{data_dir}/feacher_ing'
 
 process_raw_data_etfs = SparkSubmitOperator(
     task_id='process_raw_data_etfs',
+    conn_id= 'spark_local',
     application=f'{scripts_dir}/process_raw_data.py',
     application_args=[data_dir, '200', 'etfs'],
     dag=dag,
@@ -29,9 +42,9 @@ process_raw_data_etfs = SparkSubmitOperator(
 
 feature_engineering_etfs = SparkSubmitOperator(
     task_id='feature_engineering_etfs',
+    conn_id= 'spark_local',
     application=f'{scripts_dir}/feature_engineering.py',
-    conf={'spark.master': 'spark://spark-master:7077'},
-    application_args=[parquet_file_path, 'etfs'],
+    application_args=[parquet_file_path, 'etfs',output_dir],
     dag=dag,
 )
 
@@ -43,6 +56,7 @@ ml_training_etfs = BashOperator(
 
 process_raw_data_stocks = SparkSubmitOperator(
     task_id='process_raw_data_stocks',
+    conn_id= 'spark_local',
     application=f'{scripts_dir}/process_raw_data.py',
     application_args=[data_dir, '200', 'stocks'],
     dag=dag,
@@ -50,11 +64,13 @@ process_raw_data_stocks = SparkSubmitOperator(
 
 feature_engineering_stocks = SparkSubmitOperator(
     task_id='feature_engineering_stocks',
+    conn_id= 'spark_local',
     application='/path/to/feature_engineering.py',
-    conf={'spark.master': 'spark://spark-master:7077'},
-    application_args=[parquet_file_path, 'stocks'],
+    application_args=[parquet_file_path, 'stocks', output_dir],
     dag=dag,
 )
 
 process_raw_data_etfs >> feature_engineering_etfs >> ml_training_etfs
 process_raw_data_stocks >> feature_engineering_stocks
+#if __name__ == "__main__":
+#    dag.cli()
