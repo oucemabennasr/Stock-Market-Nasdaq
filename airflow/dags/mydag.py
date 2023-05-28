237829ec242b294f18f6d95308956bba702b8977
oucema <cloud_user@4b5d28de6f1c.mylabserver.com>
2023-05-28 22:52:58 +0000

    add another dagg

--- airflow/dags/mydag.py
+++ airflow/dags/mydag.py
@@ -40,7 +40,7 @@ process_raw_data_etfs = BashOperator(
 
 feature_engineering_etfs = BashOperator(
     task_id='feature_engineering_etfs',
-    bash_command=f'spark-submit --master local[*] --name arrow-spark --queue root.default {scripts_dir}/feature_engineering.py {parquet_file_path} etfs {output_dir} || true',
+    bash_command=f'spark-submit {scripts_dir}/feature_engineering.py {parquet_file_path} etfs {output_dir} || true',
     trigger_rule='one_success',
     dag=dag,
 )
@@ -52,5 +52,27 @@ ml_training_etfs = BashOperator(
     dag=dag,
 )
 
+process_raw_data_stocks = BashOperator(
+    task_id='process_raw_data_stocks',
+    bash_command=f'spark-submit {scripts_dir}/process_raw_data.py {data_dir} 20 stocks || true',
+    trigger_rule='one_success',
+    dag=dag,
+)
+
+feature_engineering_stocks = BashOperator(
+    task_id='feature_engineering_stocks'',
+    bash_command=f'spark-submit {scripts_dir}/feature_engineering.py {parquet_file_path} stocks {output_dir} || true',
+    trigger_rule='one_success',
+    dag=dag,
+)
+
+ml_training_etfs = BashOperator(
+    task_id='ml_training_stocks'',
+    bash_command=f'{scripts_dir}/ml_training || true',
+    trigger_rule='all_success',
+    dag=dag,
+)
+
 
 process_raw_data_etfs >> feature_engineering_etfs >> ml_training_etfs
+process_raw_data_stocks >> feature_engineering_stocks >> ml_training_stocks
