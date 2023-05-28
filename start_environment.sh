92fbce42ea3339e0e8ef9c1a2640b5ebf34f26be
Oussama Ben Nasr <96354393+oucemabennasr@users.noreply.github.com>
2023-05-27 23:28:05 -0400

    Update start_environment.sh

--- start_environment.sh
+++ start_environment.sh
@@ -4,8 +4,18 @@ sudo apt-get upgrade
 sudo apt-get install python3 python3-dev
 sudo apt-get install openjdk-17-jdk
 sudo apt install python3-pip
+sudo pip3 install virtualenv
+virtualenv airflow_env
+source airflow_env/bin/activate
+pip3 install apache-airflow[sentry,statsd]
+mkdir airflow
+cd airflow
+airflow db init
+mkdir dags
 sudo pip3 install pandas
 sudo pip3 install pyarrow
 sudo pip3 install pyspark
 sudo pip3 install -U scikit-learn
 pip install xgboost
+airflow users create --username oucema --firstname  oussama --lastname BenNasr --role Admin --email my@example.com --password mypass
+airflow scheduler
