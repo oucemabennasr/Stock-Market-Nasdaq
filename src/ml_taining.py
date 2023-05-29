f358561982dcff550ebc66a2075c1e5e24a754f5
Oussama Ben Nasr <96354393+oucemabennasr@users.noreply.github.com>
2023-05-29 02:42:36 -0400

    Update ml_taining.py

--- src/ml_taining.py
+++ src/ml_taining.py
@@ -1,12 +1,18 @@
+import argparse
 import pandas as pd
 import xgboost as xgb
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_absolute_error, mean_squared_error
+import pickle
 import logging
 
-data = pd.read_parquet('/home/cloud_user/Stock-Market-Nasdaq/data/feacher_ing/etfs.parquet')
+parser.add_argument('data', type=str, choices=['etfs', 'stocks'], help='Data to process (etfs or stocks)')
+args = parser.parse_args()
+d = args.data
 
-# Assume `data` is loaded as a Pandas DataFrame
+data = pd.read_parquet('/home/oucemabennasr/Stock-Market-Nasdaq/data/processed_data/parquet_format_avg_med/{d}.parquet')
+
+
 data['Date'] = pd.to_datetime(data['Date'])
 data.set_index('Date', inplace=True)
 
@@ -40,14 +46,16 @@ absolute_percentage_diff = abs(y_test - y_pred) / y_test
 accurate_predictions = absolute_percentage_diff[absolute_percentage_diff <= threshold].count()
 accuracy = (accurate_predictions / len(y_test)) * 100
 
-# Define the version number
 version = '1.0'
 
 # Save the trained model to disk with the version
-model.save_model(f'/home/cloud_user/Stock-Market-Nasdaq/model/model_{version}.bin')
+file_path = f'/home/oucemabennasr/Stock-Market-Nasdaq/model/model_{d}_{version}.pkl'
 
+model.save_model(f'/home/cloud_user/Stock-Market-Nasdaq/model/model_{version}.bin')
+with open(file_path, 'wb') as f:
+    pickle.dump(model, f)
 # Set up logging
-logging.basicConfig(filename=f'/home/cloud_user/Stock-Market-Nasdaq/model/training_metrics_{version}.log', level=logging.INFO)
+logging.basicConfig(filename=f'/home/cloud_user/Stock-Market-Nasdaq/model/training_metrics_{d}{version}.log', level=logging.INFO)
 logger = logging.getLogger()
 
 # Log training metrics
