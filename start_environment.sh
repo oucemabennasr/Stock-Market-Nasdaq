35fa04d4496c467b45eec071dac42065ce361033
Oussama Ben Nasr <96354393+oucemabennasr@users.noreply.github.com>
2023-05-29 01:50:20 -0400

    Update start_environment.sh

--- start_environment.sh
+++ start_environment.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 sudo apt-get update
 sudo apt-get upgrade
+sudo apt-get install python3 python3-dev
+sudo apt-get install openjdk-17-jdk
 sudo pip3 install virtualenv
 virtualenv airflow_env
 source airflow_env/bin/activate
-sudo apt-get install python3 python3-dev
-sudo apt-get install openjdk-17-jdk
 sudo apt install python3-pip
 sudo pip3 install pandas
 sudo pip3 install pyarrow
